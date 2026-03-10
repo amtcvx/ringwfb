968a60fc1bb2149f323b59edb772fb0a4f5071ec
pprz <you@example.com>
2026-03-10 14:57:18 +0100

    up

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 PROJ=$PWD
+DKMS=false
 mkdir $PROJ/obj $PROJ/bin
-if uname -a | grep -cs "5.10.160-legacy-rk35xx"> /dev/null 2>&1; then DKMS=true; fi
+if uname -r | grep -cs "5.10.110-15-rockchip"> /dev/null 2>&1; then DKMS=true; fi
 if $DKMS; then
-  sudo apt-get install -y dkms
+  apt-get install -y dkms
   git clone https://github.com/aircrack-ng/rtl8812au.git
   cd rtl8812au
   git checkout v5.6.4.2
--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 PROJ=$PWD
+DKMS=false
 rm -rf $PROJ/obj $PROJ/bin
-if uname -a | grep -cs "5.10.160-legacy-rk35xx"> /dev/null 2>&1; then DKMS=true; fi
+if uname -a | grep -cs "5.10.110-15-rockchip"> /dev/null 2>&1; then DKMS=true; fi
 if $DKMS; then
   drivername=`dkms status | grep 8812 | awk '{print substr($1,1,length($1)-1)}'`
   driverversion=`dkms status | grep 8812 | awk '{print substr($2,1,length($2)-1)}'`
