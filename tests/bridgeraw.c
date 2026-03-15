5790314c49314f77eaf71d8dc3bc89bc0fe50758
pprz <Vous@exemple.com>
2026-03-15 15:29:22 +0100

    up

--- tests/bridgeraw.c
+++ tests/bridgeraw.c
@@ -34,49 +34,7 @@ sudo ip link del name br0
 #include <net/if.h>
 
 #define TEST_BRIDGE_NAME "br0"
-#define TEST_INTERFACE_NAME "wlx3c7c3fa9c1e4"
-
-/************************************************************************************************/
-#define IEEE80211_RADIOTAP_MCS_HAVE_BW    0x01
-#define IEEE80211_RADIOTAP_MCS_HAVE_MCS   0x02
-#define IEEE80211_RADIOTAP_MCS_HAVE_GI    0x04
-
-#define IEEE80211_RADIOTAP_MCS_HAVE_STBC  0x20
-
-#define IEEE80211_RADIOTAP_MCS_BW_20    0
-#define IEEE80211_RADIOTAP_MCS_SGI      0x04
-
-#define IEEE80211_RADIOTAP_MCS_STBC_1  1
-#define IEEE80211_RADIOTAP_MCS_STBC_SHIFT 5
-
-#define MCS_KNOWN (IEEE80211_RADIOTAP_MCS_HAVE_MCS | IEEE80211_RADIOTAP_MCS_HAVE_BW | IEEE80211_RADIOTAP_MCS_HAVE_GI | IEEE80211_RADIOTAP_MCS_HAVE_STBC )
-
-#define MCS_FLAGS  (IEEE80211_RADIOTAP_MCS_BW_20 | IEEE80211_RADIOTAP_MCS_SGI | (IEEE80211_RADIOTAP_MCS_STBC_1 << IEEE80211_RADIOTAP_MCS_STBC_SHIFT))
-
-#define MCS_INDEX  2
-
-/************************************************************************************************/
-
-uint8_t radiotaphd_tx[] = {
-        0x00, 0x00, // <-- radiotap version
-        0x0d, 0x00, // <- radiotap header length
-        0x00, 0x80, 0x08, 0x00, // <-- radiotap present flags:  RADIOTAP_TX_FLAGS + RADIOTAP_MCS
-        0x08, 0x00,  // RADIOTAP_F_TX_NOACK
-        MCS_KNOWN , MCS_FLAGS, MCS_INDEX // bitmap, flags, mcs_index
-};
-uint8_t ieeehd_tx[] = {
-        0x08, 0x01,                         // Frame Control : Data frame from STA to DS
-        0x00, 0x00,                         // Duration
-        0x66, 0x55, 0x44, 0x33, 0x22, 0x11, // Receiver MAC
-        0x66, 0x55, 0x44, 0x33, 0x22, 0x11, // Transmitter MAC
-        0x66, 0x55, 0x44, 0x33, 0x22, 0x11, // Destination MAC
-        0x10, 0x86                          // Sequence control
-};
-uint8_t llchd_tx[4];
-
-struct iovec iov_radiotaphd_tx = { .iov_base = radiotaphd_tx, .iov_len = sizeof(radiotaphd_tx)};
-struct iovec iov_ieeehd_tx =     { .iov_base = ieeehd_tx,     .iov_len = sizeof(ieeehd_tx)};
-struct iovec iov_llchd_tx =      { .iov_base = llchd_tx,      .iov_len = sizeof(llchd_tx)};
+#define TEST_INTERFACE_NAME "wlx3c7c3fa9bdc6"
 
 /*****************************************************************************/
 int main(int argc, char *argv[]) {
@@ -93,45 +51,39 @@ int main(int argc, char *argv[]) {
   if (nl_connect(sockrt, NETLINK_ROUTE)) exit(-1);
 
   struct nl_cache *cache;
-  if ((rtnl_link_alloc_cache(sockrt, AF_UNSPEC, &cache)) < 0) exit(-1);
+  if ((rtnl_link_alloc_cache(sockrt, sockid, &cache)) < 0) exit(-1);
+
   struct rtnl_link *ltap;
   if (!(ltap = rtnl_link_get_by_name(cache, TEST_INTERFACE_NAME))) exit(-1);
-  uint16_t index = rtnl_link_get_ifindex(ltap);
 
+  struct rtnl_link *change;
+  if (!(change = rtnl_link_alloc())) exit(-1);
+  rtnl_link_unset_flags(change, IFF_UP);
+  if ((rtnl_link_change(sockrt, ltap, change, 0)) < 0) exit(-1);
+
+
+  uint16_t index = rtnl_link_get_ifindex(ltap);
   struct nl_msg *nlmsg;
-  if (!(nlmsg  = nlmsg_alloc())) exit(-1);;
+  if (!(nlmsg  = nlmsg_alloc())) exit(-1);
   genlmsg_put(nlmsg,0,0,sockid,0,0,NL80211_CMD_SET_INTERFACE,0);  //  DOWN interfaces
   nla_put_u32(nlmsg, NL80211_ATTR_IFINDEX, index);
   nla_put_u8(nlmsg, NL80211_ATTR_4ADDR,1);
+//  nla_put_u32(nlmsg, NL80211_ATTR_IFTYPE,NL80211_IFTYPE_MONITOR);
   nl_send_auto(socknl, nlmsg);
   if (nl_send_auto(socknl, nlmsg) >= 0)  nl_recvmsgs_default(socknl);
   nlmsg_free(nlmsg);
 
 
   if ((rtnl_link_bridge_add(sockrt, TEST_BRIDGE_NAME)) < 0) exit(-1);
-
   if ((rtnl_link_alloc_cache(sockrt, AF_UNSPEC, &cache)) < 0) exit(-1);
   struct rtnl_link *link;
   if (!(link = rtnl_link_alloc ())) exit(-1);
   if (!(link = rtnl_link_get_by_name(cache, TEST_BRIDGE_NAME))) exit(-1);
-
   if ((rtnl_link_enslave(sockrt, link, ltap)) < 0) exit(-1);
-
-
-  if (!(nlmsg  = nlmsg_alloc())) exit(-1);;
-  nla_put_u32(nlmsg, NL80211_ATTR_IFINDEX, index);
-  nla_put_u32(nlmsg, NL80211_ATTR_IFTYPE,NL80211_IFTYPE_MONITOR);
-  nl_send_auto(socknl, nlmsg);
-  if (nl_send_auto(socknl, nlmsg) >= 0)  nl_recvmsgs_default(socknl);
-  nlmsg_free(nlmsg);
-
-  struct rtnl_link *change;
-  if (!(change = rtnl_link_alloc ())) exit(-1);
-  if (rtnl_link_alloc_cache(sockrt, AF_UNSPEC, &cache) < 0) exit(-1);
-  if (!(link = rtnl_link_get(cache,index))) exit(-1);
-  if (!(change = rtnl_link_alloc ())) exit(-1);
-  rtnl_link_set_flags (change, IFF_UP);
-  rtnl_link_change(sockrt, link, change, 0);
+  
+  if (!(change = rtnl_link_alloc())) exit(-1);
+  rtnl_link_set_flags(change, IFF_UP);
+  if ((rtnl_link_change(sockrt, ltap, change, 0)) < 0) exit(-1);
 
   printf("HELLO (%d)\n",index);
 
