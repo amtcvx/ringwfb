742568fe3606ed2a2cf47c6c666fe3ff1b5eb538
pprz <you@example.com>
2026-03-17 11:21:39 +0100

    up

--- tests/bridgeraw.c
+++ tests/bridgeraw.c
@@ -18,87 +18,40 @@ sudo ip link show master br0
 sudo ip link del name br0
 
 */
+#include<unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
+#include <poll.h>
+#include <sys/uio.h>
+
+#include <net/ethernet.h>
+#include <net/if.h>
+#include <sys/ioctl.h>
 #include <linux/if_packet.h>
-#include <net/ethernet.h> 
+#include <linux/filter.h>
 
 #include <stdbool.h>
-
-
-#include <linux/netlink.h>
-
 #include <netlink/netlink.h>
-
 #include <netlink/genl/genl.h>
 #include <netlink/genl/family.h>
-#include <netlink/genl/ctrl.h> 
+#include <netlink/genl/ctrl.h>
 #include <linux/nl80211.h>
 
 #include <netlink/route/link.h>
-#include <netlink/route/link/bridge.h>
-
 #include <net/if.h>
+
 #include <sys/timerfd.h>
-#include <unistd.h>
 
+#include <errno.h>
 
-#define TEST_BRIDGE_NAME "br0"
-char *rawnames[] = { "wlxfc349725a319","wlx3c7c3fa9c1e8" };
-uint32_t rawfreqs[] = { 2472,5280 };
-
-/************************************************************************************************/
-#define IEEE80211_RADIOTAP_MCS_HAVE_BW    0x01
-#define IEEE80211_RADIOTAP_MCS_HAVE_MCS   0x02
-#define IEEE80211_RADIOTAP_MCS_HAVE_GI    0x04
-
-#define IEEE80211_RADIOTAP_MCS_HAVE_STBC  0x20
-
-#define IEEE80211_RADIOTAP_MCS_BW_20    0
-#define IEEE80211_RADIOTAP_MCS_SGI      0x04
-
-#define IEEE80211_RADIOTAP_MCS_STBC_1  1
-#define IEEE80211_RADIOTAP_MCS_STBC_SHIFT 5
-
-#define MCS_KNOWN (IEEE80211_RADIOTAP_MCS_HAVE_MCS | IEEE80211_RADIOTAP_MCS_HAVE_BW | IEEE80211_RADIOTAP_MCS_HAVE_GI | IEEE80211_RADIOTAP_MCS_HAVE_STBC )
-
-#define MCS_FLAGS  (IEEE80211_RADIOTAP_MCS_BW_20 | IEEE80211_RADIOTAP_MCS_SGI | (IEEE80211_RADIOTAP_MCS_STBC_1 << IEEE80211_RADIOTAP_MCS_STBC_SHIFT))
-
-#define MCS_INDEX  2
-
-/************************************************************************************************/
-
-uint8_t radiotaphd_tx[] = {
-        0x00, 0x00, // <-- radiotap version
-        0x0d, 0x00, // <- radiotap header length
-        0x00, 0x80, 0x08, 0x00, // <-- radiotap present flags:  RADIOTAP_TX_FLAGS + RADIOTAP_MCS
-        0x08, 0x00,  // RADIOTAP_F_TX_NOACK
-        MCS_KNOWN , MCS_FLAGS, MCS_INDEX // bitmap, flags, mcs_index
-};
-uint8_t ieeehd_tx[] = {
-        0x08, 0x01,                         // Frame Control : Data frame from STA to DS
-        0x00, 0x00,                         // Duration
-        0x66, 0x55, 0x44, 0x33, 0x22, 0x11, // Receiver MAC
-        0x66, 0x55, 0x44, 0x33, 0x22, 0x11, // Transmitter MAC
-        0x66, 0x55, 0x44, 0x33, 0x22, 0x11, // Destination MAC
-        0x10, 0x86                          // Sequence control
-};
-uint8_t llchd_tx[4];
-
-struct iovec iov_radiotaphd_tx = { .iov_base = radiotaphd_tx, .iov_len = sizeof(radiotaphd_tx)};
-struct iovec iov_ieeehd_tx =     { .iov_base = ieeehd_tx,     .iov_len = sizeof(ieeehd_tx)};
-struct iovec iov_llchd_tx =      { .iov_base = llchd_tx,      .iov_len = sizeof(llchd_tx)};
+char *rawnames[] = { "wlx3c7c3fa9c1e4" };
+uint32_t rawfreqs[] = { 2412 };
 
 /*****************************************************************************/
-
-typedef struct {
-  uint8_t droneid;
-  uint8_t type;
-  uint16_t len;
-  uint32_t seq;
-} __attribute__((packed)) wfb_utils_heads_pay_t;
-
-#define PAY_MTU 1400
-#define ONLINE_MTU PAY_MTU + sizeof(wfb_utils_heads_pay_t)
 #define PERIOD_DELAY_S  1
 
 /*****************************************************************************/
@@ -109,79 +62,6 @@ void init(uint8_t *sockid, struct nl_sock **sockrt, struct nl_sock **socknl) {
   if ((*sockid = genl_ctrl_resolve(*socknl, "nl80211")) < 0) exit(-1);
   if (!(*sockrt = nl_socket_alloc())) exit(-1);
   if (nl_connect(*sockrt, NETLINK_ROUTE)) exit(-1);
-}
-
-/*****************************************************************************/
-void preset(uint8_t sockid, struct nl_sock *sockrt, struct nl_sock *socknl, char *name,  uint16_t *index) {
-
-  struct rtnl_link *ltap;
-  struct nl_cache *cache;
-  if ((rtnl_link_alloc_cache(sockrt, sockid, &cache)) < 0) exit(-1);
-  if (!(ltap = rtnl_link_get_by_name(cache, name))) exit(-1);
-  struct rtnl_link *change;
-  if (!(change = rtnl_link_alloc())) exit(-1);
-  rtnl_link_unset_flags(change, IFF_UP);
-  if ((rtnl_link_change(sockrt, ltap, change, 0)) < 0) exit(-1);
-
-  *index = rtnl_link_get_ifindex(ltap);
-  struct nl_msg *nlmsg;
-
-  if (!(nlmsg  = nlmsg_alloc())) exit(-1);
-  genlmsg_put(nlmsg,0,0,sockid,0,0,NL80211_CMD_SET_INTERFACE,0);  //  DOWN interfaces
-  nla_put_u32(nlmsg, NL80211_ATTR_IFINDEX, *index);
-  nla_put_u8(nlmsg, NL80211_ATTR_4ADDR,1);
-  nl_send_auto(socknl, nlmsg);
-  if (nl_send_auto(socknl, nlmsg) >= 0)  nl_recvmsgs_default(socknl);
-  nlmsg_free(nlmsg);
-}
-
-/*****************************************************************************/
-void set(struct nl_sock *sockrt, struct rtnl_link **link) {
-  struct nl_cache *cache;
-
-  if ((rtnl_link_bridge_add(sockrt, TEST_BRIDGE_NAME)) < 0) exit(-1);
-  if ((rtnl_link_alloc_cache(sockrt, AF_UNSPEC, &cache)) < 0) exit(-1);
-  if (!(*link = rtnl_link_alloc ())) exit(-1);
-  if (!(*link = rtnl_link_get_by_name(cache, TEST_BRIDGE_NAME))) exit(-1);
-}
-
-/*****************************************************************************/
-void postset(uint8_t sockid, struct nl_sock *sockrt, struct nl_sock *socknl,  uint16_t index, 
-  struct rtnl_link *link) {
-  
-  struct rtnl_link *ltap;
-  struct nl_cache *cache;
-  if ((rtnl_link_alloc_cache(sockrt, sockid, &cache)) < 0) exit(-1);
-  if (!(ltap = rtnl_link_get(cache, index))) exit(-1);
-
-  if ((rtnl_link_enslave(sockrt, link, ltap)) < 0) exit(-1);
-
-  struct nl_msg *nlmsg;
-  if (!(nlmsg  = nlmsg_alloc())) exit(-1);
-  genlmsg_put(nlmsg,0,0,sockid,0,0,NL80211_CMD_SET_INTERFACE,0);  //  DOWN interfaces
-  nla_put_u32(nlmsg, NL80211_ATTR_IFINDEX, index);
-  nla_put_u32(nlmsg, NL80211_ATTR_IFTYPE,NL80211_IFTYPE_MONITOR);
-  nl_send_auto(socknl, nlmsg);
-  if (nl_send_auto(socknl, nlmsg) >= 0)  nl_recvmsgs_default(socknl);
-  nlmsg_free(nlmsg);
-
-  struct rtnl_link *change;
-  if (!(change = rtnl_link_alloc())) exit(-1);
-  rtnl_link_set_flags(change, IFF_UP);
-  if ((rtnl_link_change(sockrt, ltap, change, 0)) < 0) exit(-1);
-}
-
-/*****************************************************************************/
-void sockset(uint16_t index, uint8_t *sockfd) {
-
-  uint16_t protocol = 0;
-  if (-1 == (*sockfd = socket(AF_PACKET,SOCK_RAW,protocol))) exit(-1);
-  struct sockaddr_ll sll;
-  memset( &sll, 0, sizeof( sll ) );
-  sll.sll_family   = AF_PACKET;
-  sll.sll_ifindex  = index;
-  sll.sll_protocol = protocol;
-  if (-1 == bind(*sockfd, (struct sockaddr *)&sll, sizeof(sll))) exit(-1);
 }
 
 /*****************************************************************************/
@@ -202,85 +82,87 @@ bool setfreq(uint8_t sockid, struct nl_sock *socknl, int ifindex, uint32_t freq)
 }
 
 /*****************************************************************************/
-int main(int argc, char *argv[]) {
-
-  uint8_t sockid; struct nl_sock *sockrt; struct nl_sock *socknl;
-  init(&sockid, &sockrt, &socknl);
+int main(int argc, char **argv) {
 
   uint8_t nbraws = sizeof(rawnames)/sizeof(rawnames[0]);
-
+  uint8_t nbfds = 1 + nbraws;
+  uint8_t fd[nbfds];
   struct rawdev_t { uint16_t index; char *name; } rawdev[nbraws];
   for (uint8_t i=0;i<nbraws;i++) rawdev[i].name = &rawnames[i][0];
 
-  for (uint8_t i=0;i<nbraws;i++) preset(sockid, sockrt, socknl, rawdev[i].name, &rawdev[i].index);
-
-  struct rtnl_link *link;
-  set(sockrt, &link);
-
-  for (uint8_t i=0;i<nbraws;i++) postset(sockid, sockrt, socknl, rawdev[i].index, link);
-
-  struct rtnl_link *change;
-  if (!(change = rtnl_link_alloc())) exit(-1);
-  rtnl_link_set_flags(change, IFF_UP);
-  if ((rtnl_link_change(sockrt, link, change, 0)) < 0) exit(-1);
-
-
-  for (uint8_t i=0;i<nbraws;i++) setfreq(sockid, socknl, rawdev[i].index, rawfreqs[i]);
-
-  uint8_t sockfd;
-  sockset(rtnl_link_get_ifindex(link), &sockfd);
-
-  uint8_t fd[1 + nbraws];
   uint64_t exptime;
   if (-1 == (fd[0] = timerfd_create(CLOCK_MONOTONIC, 0))) exit(-1);
   struct itimerspec period = { { PERIOD_DELAY_S, 0 }, { PERIOD_DELAY_S, 0 } };
   timerfd_settime(fd[0], 0, &period, NULL);
 
+  uint8_t sockid; struct nl_sock *sockrt; struct nl_sock *socknl;
+  init(&sockid, &sockrt, &socknl);
+
+  struct rtnl_link *ltap;
+  struct nl_cache *cache;
+  if ((rtnl_link_alloc_cache(sockrt, sockid, &cache)) < 0) exit(-1);
+  if (!(ltap = rtnl_link_get_by_name(cache, rawdev[0].name))) exit(-1);
+  struct rtnl_link *change;
+  if (!(change = rtnl_link_alloc())) exit(-1);
+  rtnl_link_unset_flags(change, IFF_UP);
+  if ((rtnl_link_change(sockrt, ltap, change, 0)) < 0) exit(-1);
+
+  uint16_t index = rtnl_link_get_ifindex(ltap);
+
+  struct nl_msg *nlmsg;
+  if (!(nlmsg  = nlmsg_alloc())) exit(-1);
+  genlmsg_put(nlmsg,0,0,sockid,0,0,NL80211_CMD_SET_INTERFACE,0);  //  DOWN interfaces
+  nla_put_u32(nlmsg, NL80211_ATTR_IFINDEX, index);
+  nla_put_u32(nlmsg, NL80211_ATTR_IFTYPE,NL80211_IFTYPE_MONITOR);
+  nl_send_auto(socknl, nlmsg);
+  if (nl_send_auto(socknl, nlmsg) >= 0)  nl_recvmsgs_default(socknl);
+  nlmsg_free(nlmsg);
+
+  if (!(change = rtnl_link_alloc())) exit(-1);
+  rtnl_link_set_flags(change, IFF_UP);
+  if ((rtnl_link_change(sockrt, ltap, change, 0)) < 0) exit(-1);
+
+
+  uint16_t protocol = htons(ETH_P_ALL);
+  if (-1 == (fd[1] = socket(AF_PACKET,SOCK_RAW,protocol))) exit(-1);
+
+  struct sockaddr_ll sll;
+  memset( &sll, 0, sizeof( sll ) );
+  sll.sll_family   = AF_PACKET;
+  sll.sll_ifindex  = index;
+  sll.sll_protocol = protocol;
+  if (-1 == bind(fd[1], (struct sockaddr *)&sll, sizeof(sll))) exit(-1); // BIND must be AFTER wifi setting
+
   const int32_t sock_qdisc_bypass = 1;
-  for (uint8_t i=0;i<nbraws;i++) { 
-    sockset(rawdev[i].index, &fd[1 + i]);
-    if (-1 == setsockopt(fd[1 + i], SOL_PACKET, PACKET_QDISC_BYPASS, &sock_qdisc_bypass, sizeof(sock_qdisc_bypass))) exit(-1);
-  }
+  if (-1 == setsockopt(fd[1], SOL_PACKET, PACKET_QDISC_BYPASS, &sock_qdisc_bypass, sizeof(sock_qdisc_bypass))) exit(-1);
 
-  uint8_t dumbuf[1400] = {-1};
-  struct iovec iovdum = { .iov_base = dumbuf, .iov_len = sizeof(dumbuf) };
-  struct iovec iovtab[4] = { iov_radiotaphd_tx, iov_ieeehd_tx, iov_llchd_tx, iovdum };
-  struct msghdr msg = { .msg_iov = iovtab, .msg_iovlen = 4 };
+  uint8_t dumbuf[1500];
+  struct iovec iov_dum = { .iov_base = dumbuf, .iov_len = sizeof(dumbuf)};
+  struct iovec iovtab[1] = { iov_dum };
+  struct msghdr msg = { .msg_iov = iovtab, .msg_iovlen = 1 };
 
-  const uint8_t nfds = 3;
-  struct pollfd readsets[3] = { { .fd = fd[0], .events = POLLIN }, { .fd = fd[1], .events = POLLIN }, { .fd = fd[2], .events = POLLIN }};
+  ssize_t rawlen = 0, len = 0;
+  uint8_t rawnb = 0;
 
-  uint8_t payloadbuf_in[ONLINE_MTU];
+  setfreq(sockid, socknl, index, 2484);
 
-  wfb_utils_heads_pay_t headspay;
-  struct iovec iovheadpay = { .iov_base = &headspay, .iov_len = sizeof(wfb_utils_heads_pay_t) };
-
-  ssize_t len;
+  struct pollfd readsets[nbfds];
+  for (uint8_t i=0; i<nbfds; i++) { readsets[i].fd = fd[i]; readsets[i].events = POLLIN; }
 
   for(;;) {
-    if (0 != poll(readsets, nfds, -1)) {
-      for (uint8_t i=0; i<nfds; i++) {
-        if (readsets[i].revents == POLLIN) {
-          if (i == 0) {
+    if (0 != poll(readsets, 2, -1)) {
+      for (uint8_t cpt=0; cpt<2; cpt++) {
+        if (readsets[cpt].revents == POLLIN) {
+          if (cpt == 0) {
             len = read(fd[0], &exptime, sizeof(uint64_t));
-            ssize_t rawlen = sendmsg(sockfd, (const struct msghdr *)&msg, MSG_DONTWAIT);
-            printf("(%ld)\n",rawlen);
-	  } else  {
-            printf("(%d)\n",i);
-
-            memset(&headspay,0,sizeof(wfb_utils_heads_pay_t));
-            memset(&payloadbuf_in,0,sizeof(payloadbuf_in));
-            struct iovec iovpay = { .iov_base =  payloadbuf_in, .iov_len = sizeof(payloadbuf_in) };
-            struct iovec iovpart[2] = { iovheadpay, iovpay };
-            struct msghdr msg = { .msg_iov = iovpart, .msg_iovlen=2 };
-
-            if ((len = recvmsg(fd[i], &msg, MSG_DONTWAIT)) > 0) {
-	    }
+	    printf("[%d]\n",rawnb);
+	    rawnb = 0;
+	  } else {
+            rawlen = recvmsg(fd[1], &msg, MSG_DONTWAIT);
+	    rawnb++;
 	  }
 	}
       }
     }
   }
-
-  return 0;
 }
