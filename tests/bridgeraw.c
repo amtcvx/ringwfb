a0a6262f1472ede34eb7035375e7bbbe72acc55f
pprz <you@example.com>
2026-03-17 15:20:31 +0100

    up

--- tests/bridgeraw.c
+++ tests/bridgeraw.c
@@ -1,7 +1,4 @@
 /*
-
-https://serverfault.com/questions/152363/bridging-wlan0-to-eth0
-
 sudo apt-get install libnl-3-dev libnl-genl-3-dev libnl-route-3-dev
 
 gcc -g -O2 -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs -fno-strict-aliasing -fno-common -Werror-implicit-function-declaration -DCONFIG_LIBNL30 -I/usr/include/libnl3 -c bridgeraw.c -o bridgeraw.o
@@ -10,7 +7,11 @@ cc bridgeraw.o -g -lnl-route-3 -lnl-genl-3 -lnl-3 -o exe_bridgeraw
 
 sudo rfkill unblock ...
 
+!! will not get packet after first plugin !!
+
 sudo ./exe_bridgeraw 
+
+sudo ip link add name br0 type bridge
 
 sudo ip address show br0
 sudo ip link show master br0
@@ -42,13 +43,17 @@ sudo ip link del name br0
 #include <linux/nl80211.h>
 
 #include <netlink/route/link.h>
+#include <netlink/route/link/bridge.h>
+
 #include <net/if.h>
 
 #include <sys/timerfd.h>
 
 #include <errno.h>
 
-char *rawnames[] = { "wlx3c7c3fa9c1e4" };
+#define TEST_BRIDGE_NAME "br0"
+
+char *rawnames[] = { "wlx3c7c3fa9bdca" };
 uint32_t rawfreqs[] = { 2412 };
 
 /*****************************************************************************/
@@ -82,6 +87,86 @@ bool setfreq(uint8_t sockid, struct nl_sock *socknl, int ifindex, uint32_t freq)
 }
 
 /*****************************************************************************/
+void preset(uint8_t sockid, struct nl_sock *sockrt, struct nl_sock *socknl, char *name,  uint16_t *index) {
+
+  struct rtnl_link *ltap;
+  struct nl_cache *cache;
+  if ((rtnl_link_alloc_cache(sockrt, sockid, &cache)) < 0) exit(-1);
+  if (!(ltap = rtnl_link_get_by_name(cache, name))) exit(-1);
+  struct rtnl_link *change;
+  if (!(change = rtnl_link_alloc())) exit(-1);
+  rtnl_link_unset_flags(change, IFF_UP);
+  if ((rtnl_link_change(sockrt, ltap, change, 0)) < 0) exit(-1);
+  *index = rtnl_link_get_ifindex(ltap);
+
+  struct nl_msg *nlmsg;
+  if (!(nlmsg  = nlmsg_alloc())) exit(-1);
+  genlmsg_put(nlmsg,0,0,sockid,0,0,NL80211_CMD_SET_INTERFACE,0);  //  DOWN interfaces
+  nla_put_u32(nlmsg, NL80211_ATTR_IFINDEX, *index);
+  nla_put_u8(nlmsg, NL80211_ATTR_4ADDR,1);
+  nl_send_auto(socknl, nlmsg);
+  if (nl_send_auto(socknl, nlmsg) >= 0)  nl_recvmsgs_default(socknl);
+  nlmsg_free(nlmsg);
+
+}
+
+/*****************************************************************************/
+void postset(uint8_t sockid, struct nl_sock *sockrt, struct nl_sock *socknl,  uint16_t index,
+  struct rtnl_link *link) {
+
+  struct rtnl_link *ltap;
+  struct nl_cache *cache;
+  if ((rtnl_link_alloc_cache(sockrt, sockid, &cache)) < 0) exit(-1);
+  if (!(ltap = rtnl_link_get(cache, index))) exit(-1);
+
+  if ((rtnl_link_enslave(sockrt, link, ltap)) < 0) exit(-1);
+
+  struct nl_msg *nlmsg;
+  if (!(nlmsg  = nlmsg_alloc())) exit(-1);
+  genlmsg_put(nlmsg,0,0,sockid,0,0,NL80211_CMD_SET_INTERFACE,0);  //  DOWN interfaces
+  nla_put_u32(nlmsg, NL80211_ATTR_IFINDEX, index);
+  nla_put_u32(nlmsg, NL80211_ATTR_IFTYPE,NL80211_IFTYPE_MONITOR);
+  nl_send_auto(socknl, nlmsg);
+  if (nl_send_auto(socknl, nlmsg) >= 0)  nl_recvmsgs_default(socknl);
+  nlmsg_free(nlmsg);
+
+  struct rtnl_link *change;
+  if (!(change = rtnl_link_alloc())) exit(-1);
+  rtnl_link_set_flags(change, IFF_UP);
+  if ((rtnl_link_change(sockrt, ltap, change, 0)) < 0) exit(-1);
+}
+
+/*****************************************************************************/
+void set(struct nl_sock *sockrt, struct rtnl_link **link) {
+
+  struct nl_cache *cache;
+  if ((rtnl_link_bridge_add(sockrt, TEST_BRIDGE_NAME)) < 0) exit(-1);
+  if ((rtnl_link_alloc_cache(sockrt, AF_UNSPEC, &cache)) < 0) exit(-1);
+  if (!(*link = rtnl_link_alloc ())) exit(-1);
+
+  struct nl_cache *cache;
+  if ((rtnl_link_alloc_cache(sockrt, AF_UNSPEC, &cache)) < 0) exit(-1);
+  if (!(*link = rtnl_link_get_by_name(cache, TEST_BRIDGE_NAME))) exit(-1);
+}
+
+/*****************************************************************************/
+void sockset(uint16_t index, uint8_t *fd) {
+
+  uint16_t protocol = htons(ETH_P_ALL);
+  if (-1 == (*fd = socket(AF_PACKET,SOCK_RAW,protocol))) exit(-1);
+
+  struct sockaddr_ll sll;
+  memset( &sll, 0, sizeof( sll ) );
+  sll.sll_family   = AF_PACKET;
+  sll.sll_ifindex  = index;
+  sll.sll_protocol = protocol;
+  if (-1 == bind(*fd, (struct sockaddr *)&sll, sizeof(sll))) exit(-1); // BIND must be AFTER wifi setting
+
+  const int32_t sock_qdisc_bypass = 1;
+  if (-1 == setsockopt(*fd, SOL_PACKET, PACKET_QDISC_BYPASS, &sock_qdisc_bypass, sizeof(sock_qdisc_bypass))) exit(-1);
+}
+
+/*****************************************************************************/
 int main(int argc, char **argv) {
 
   uint8_t nbraws = sizeof(rawnames)/sizeof(rawnames[0]);
@@ -98,53 +183,24 @@ int main(int argc, char **argv) {
   uint8_t sockid; struct nl_sock *sockrt; struct nl_sock *socknl;
   init(&sockid, &sockrt, &socknl);
 
-  struct rtnl_link *ltap;
-  struct nl_cache *cache;
-  if ((rtnl_link_alloc_cache(sockrt, sockid, &cache)) < 0) exit(-1);
-  if (!(ltap = rtnl_link_get_by_name(cache, rawdev[0].name))) exit(-1);
-  struct rtnl_link *change;
-  if (!(change = rtnl_link_alloc())) exit(-1);
-  rtnl_link_unset_flags(change, IFF_UP);
-  if ((rtnl_link_change(sockrt, ltap, change, 0)) < 0) exit(-1);
+  for(uint8_t i=0; i<nbraws; i++) preset(sockid, sockrt, socknl, rawdev[i].name, &rawdev[i].index );
 
-  uint16_t index = rtnl_link_get_ifindex(ltap);
+  struct rtnl_link *link;
+  set(sockrt, &link);
 
-  struct nl_msg *nlmsg;
-  if (!(nlmsg  = nlmsg_alloc())) exit(-1);
-  genlmsg_put(nlmsg,0,0,sockid,0,0,NL80211_CMD_SET_INTERFACE,0);  //  DOWN interfaces
-  nla_put_u32(nlmsg, NL80211_ATTR_IFINDEX, index);
-  nla_put_u32(nlmsg, NL80211_ATTR_IFTYPE,NL80211_IFTYPE_MONITOR);
-  nl_send_auto(socknl, nlmsg);
-  if (nl_send_auto(socknl, nlmsg) >= 0)  nl_recvmsgs_default(socknl);
-  nlmsg_free(nlmsg);
+  for (uint8_t i=0; i<nbraws; i++) postset(sockid, sockrt, socknl, rawdev[i].index, link);
 
-  if (!(change = rtnl_link_alloc())) exit(-1);
-  rtnl_link_set_flags(change, IFF_UP);
-  if ((rtnl_link_change(sockrt, ltap, change, 0)) < 0) exit(-1);
-
-
-  uint16_t protocol = htons(ETH_P_ALL);
-  if (-1 == (fd[1] = socket(AF_PACKET,SOCK_RAW,protocol))) exit(-1);
-
-  struct sockaddr_ll sll;
-  memset( &sll, 0, sizeof( sll ) );
-  sll.sll_family   = AF_PACKET;
-  sll.sll_ifindex  = index;
-  sll.sll_protocol = protocol;
-  if (-1 == bind(fd[1], (struct sockaddr *)&sll, sizeof(sll))) exit(-1); // BIND must be AFTER wifi setting
-
-  const int32_t sock_qdisc_bypass = 1;
-  if (-1 == setsockopt(fd[1], SOL_PACKET, PACKET_QDISC_BYPASS, &sock_qdisc_bypass, sizeof(sock_qdisc_bypass))) exit(-1);
+  for (uint8_t i=0; i<nbraws; i++) sockset(rawdev[i].index, &fd[i + 1]);
 
   uint8_t dumbuf[1500];
   struct iovec iov_dum = { .iov_base = dumbuf, .iov_len = sizeof(dumbuf)};
   struct iovec iovtab[1] = { iov_dum };
   struct msghdr msg = { .msg_iov = iovtab, .msg_iovlen = 1 };
 
-  ssize_t rawlen = 0, len = 0;
-  uint8_t rawnb = 0;
+  ssize_t len = 0;
+  uint8_t rawpkt = 0;
 
-  setfreq(sockid, socknl, index, 2484);
+  for (uint8_t i=0; i<nbraws; i++) setfreq(sockid, socknl, rawdev[i].index, 2484);
 
   struct pollfd readsets[nbfds];
   for (uint8_t i=0; i<nbfds; i++) { readsets[i].fd = fd[i]; readsets[i].events = POLLIN; }
@@ -155,11 +211,11 @@ int main(int argc, char **argv) {
         if (readsets[cpt].revents == POLLIN) {
           if (cpt == 0) {
             len = read(fd[0], &exptime, sizeof(uint64_t));
-	    printf("[%d]\n",rawnb);
-	    rawnb = 0;
+	    printf("[%d]\n",rawpkt);
+	    rawpkt = 0;
 	  } else {
-            rawlen = recvmsg(fd[1], &msg, MSG_DONTWAIT);
-	    rawnb++;
+            len = recvmsg(fd[1], &msg, MSG_DONTWAIT);
+	    rawpkt++;
 	  }
 	}
       }
