9d59cc1f09ce2616672b931fdeeabd0318320478
pprz <Vous@exemple.com>
2026-04-07 14:57:20 +0200

    up

--- src/wfb_sync.c
+++ src/wfb_sync.c
@@ -14,9 +14,12 @@
 /******************************************************************************/
 void periodic_slave(wfb_sync_init_t *s, wfb_netlink_init_t *n, wfb_log_init_t *l) {
 
+  printf("slave\n");fflush(stdout);
+
   for (uint8_t i=0; i<n->nbraws; i++) {
     int8_t up = -1;
-    if (s->cptack[i] >= 1) { 
+    if (s->cptack[i] >= ACKTIME_S) { 
+      s->cptack[i] = 1;
 
       up = i;
       if ((++(n->rawdevs[i]->cptfreq)) >= (n->rawdevs[i]->nbfreqs)) n->rawdevs[i]->cptfreq = 0;
@@ -27,30 +30,32 @@ void periodic_slave(wfb_sync_init_t *s, wfb_netlink_init_t *n, wfb_log_init_t *l
       }
 
     } else {
+      if (s->cptack[i] == 0) {
 
-      if (s->backfreq[i] == 0) { s->fdmain = i; s->fdback = -1; }
-      else {
+        if (s->backfreq[i] == 0) { s->fdmain = i; s->fdback = -1; }
+        else {
 
-        uint8_t j=0;
-        for (j=0; j < n->rawdevs[j]->nbfreqs; j++) {
-          if (n->rawdevs[i]->freqs[j] == abs(s->backfreq[i])) break;
+          uint8_t j=0;
+          for (j=0; j < n->rawdevs[j]->nbfreqs; j++) {
+            if (n->rawdevs[i]->freqs[j] == abs(s->backfreq[i])) break;
+          }
+          if (n->rawdevs[i]->freqs[j] == s->backfreq[i]) {
+
+            uint8_t k=0;
+            if (s->backfreq[i] > 0) { s->fdmain = i; for (k=0; k<n->nbraws; k++) if (k != i) s->fdback = k; }
+            else if (s->backfreq[i] < 0) { s->fdback = i; for (k=0; k<n->nbraws; k++) if (k != i) s->fdmain = k; up = k; }
+
+            (n->rawdevs[up]->cptfreq) = j;
+	  }
         }
-        if (n->rawdevs[i]->freqs[j] == s->backfreq[i]) {
-
-          uint8_t k=0;
-          if (s->backfreq[i] > 0) { s->fdmain = i; for (k=0; k<n->nbraws; k++) if (k != i) s->fdback = k; }
-          else if (s->backfreq[i] < 0) { s->fdback = i; for (k=0; k<n->nbraws; k++) if (k != i) s->fdmain = k; up = k; }
-
-          (n->rawdevs[up]->cptfreq) = j;
-	}
       }
-    }
+      s->cptack[i]++;
 
+    }
     if (up >= 0) wfb_netlink_setfreq(&n->sockidnl, n->rawdevs[up]->ifindex, n->rawdevs[up]->freqs[n->rawdevs[up]->cptfreq]);
   }
 
-  for (uint8_t i=0; i<n->nbraws; i++) if (s->cptack[i] < ACKTIME_S) s->cptack[i]++;
-
+/*
   if (s->fdmain >= 0) { 
     wfb_netlink_payhd_t *ptrmain = (wfb_netlink_payhd_t *)(n->msg.msg_out[s->fdmain].msg_iov[3].iov_base);
 
@@ -66,10 +71,13 @@ void periodic_slave(wfb_sync_init_t *s, wfb_netlink_init_t *n, wfb_log_init_t *l
       n->msg.msg_out[s->fdback].msg_iov[4].iov_len = 1;
     }
   }
+*/
 }
 
 /******************************************************************************/
 void periodic_master(wfb_sync_init_t *s, wfb_netlink_init_t *n, wfb_log_init_t *l) {
+
+  printf("master\n");fflush(stdout);
 
   if (s->fdmain >= 0) {
     if (s->cptfree[s->fdmain] == 0) {
