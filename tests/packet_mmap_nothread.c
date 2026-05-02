6780e2c938b5be643e6832c3f8f5c4eb42995231
pprz <Vous@exemple.com>
2026-05-02 13:33:16 +0200

    up

--- tests/packet_mmap_nothread.c
+++ tests/packet_mmap_nothread.c
@@ -28,29 +28,29 @@ gcc -g packet_mmap_nothread.c -o packet_mmap_nothread
 static sig_atomic_t sigint = 0;
 static void sighandler(int num) { sigint = 1; }
 
+struct block_desc_t {
+  uint32_t version;
+  uint32_t offset_to_priv;
+  struct tpacket_hdr_v1 h1;
+};
+
 /******************************************************************************/
 void main(int argc, char **argv) {
 
   signal(SIGINT, sighandler);
 
   if (argc != 2) exit(-1);
-//  int c_packet_sz = getpagesize();
-  printf("(%s)\n",argv[1]); //,c_packet_sz);
+  printf("(%s)\n",argv[1]);
 
   int sockfd;
   if (-1 == (sockfd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL)))) exit(-1);
-
   int tpver = TPACKET_V3;
   if (-1 == (setsockopt(sockfd, SOL_PACKET, PACKET_VERSION,  &tpver, sizeof(tpver)))) exit(-1);
-/*
-  int discard = 1;
-  if (-1 == (setsockopt(sockfd, SOL_PACKET, PACKET_LOSS, &discard, sizeof(discard)))) exit(-1);
-*/
   int on = 1;
   if (-1 == (setsockopt(sockfd, SOL_PACKET, PACKET_QDISC_BYPASS, &on, sizeof(on)))) exit(-1);
 
   /*---------------------------------------------------------------------*/
-  unsigned int block_size = 1<<12, frame_size = 1<<12, block_nr = 10;
+  unsigned int block_size = 1<<22, frame_size = 1<<11, block_nr = 64;
   unsigned int frame_nr = (block_size * block_nr) / frame_size;
 
   struct tpacket_req3 packet_req[2] = { [0 ... 1] = {
@@ -71,15 +71,18 @@ void main(int argc, char **argv) {
   /*---------------------------------------------------------------------*/
   const int c_packet_sz = 200;
   for(int i=0; i < block_nr; i++ ) {
-    struct tpacket3_hdr * ps_header = ((struct tpacket3_hdr *)((void *)map[1] + (block_size*i)));
+    struct tpacket3_hdr * tx_header = ((struct tpacket3_hdr *)((void *)map[1] + (block_size*i)));
+
+    struct block_desc_t *tx_pbd = (struct block_desc_t *) tx_header;
+    tx_pbd->h1.block_status = TP_STATUS_KERNEL;
 
     #define my_TPACKET_ALIGN(x)     (((x)+(uint64_t)(TPACKET_ALIGNMENT-1))&~((uint64_t)(TPACKET_ALIGNMENT-1)))
-    char * pkt_ptr = ((void*) ps_header) + my_TPACKET_ALIGN(sizeof(struct tpacket3_hdr));
+    char * pkt_ptr = ((void*) tx_header) + my_TPACKET_ALIGN(sizeof(struct tpacket3_hdr));
 
     for(int j=0; j<c_packet_sz; j++ ) pkt_ptr[j] = j; 
-    ps_header->tp_len = (uint32_t)c_packet_sz;
-    ps_header->tp_next_offset = 0;
-    ps_header->tp_status = TP_STATUS_KERNEL; // TP_STATUS_SEND_REQUEST;
+    tx_header->tp_len = (uint32_t)c_packet_sz;
+    tx_header->tp_next_offset = 0;
+    tx_header->tp_status = TP_STATUS_KERNEL; //TP_STATUS_SEND_REQUEST;
   }
 
   /*---------------------------------------------------------------------*/
@@ -92,11 +95,11 @@ void main(int argc, char **argv) {
 
   struct pollfd pfd = {
     .fd = sockfd,
-    .events = POLLIN,
+    .events = POLLIN | POLLOUT | POLLERR,
     .revents = 0
   };
 
-  unsigned int blockcur = 0; 
+  unsigned int rx_block_nr = 0; 
   int total_pkts = 0, ec_send, total_bytes = 0;
   bool tosend = false;
 
@@ -114,15 +117,19 @@ void main(int argc, char **argv) {
       printf("TIC\n"); fflush(stdout);
     }
 
-    for(int i=0; i < block_nr; i++ ) {
-      struct tpacket3_hdr * rx_header = ((struct tpacket3_hdr *)((void *)map[0] + (block_size*i)));
+    if (pfd.revents & POLLIN) {
+      struct tpacket3_hdr * rx_header = ((struct tpacket3_hdr *)((void *)map[0] + (block_size * rx_block_nr)));
+      struct block_desc_t *rx_pbd = (struct block_desc_t *) rx_header;
       if ((rx_header->tp_status & TP_STATUS_USER) == 0) { 
-        rx_header->tp_status = TP_STATUS_KERNEL;
+        rx_pbd->h1.block_status = TP_STATUS_KERNEL;
         printf("RECV\n"); fflush(stdout);
+        rx_block_nr = (rx_block_nr + 1) % block_nr;
       }
     }
 
+    printf("HELLO\n");
 
+/*
     for(int i=0; i < block_nr; i++ ) {
       struct tpacket3_hdr * tx_header = ((struct tpacket3_hdr *)((void *)map[1] + (block_size*i)));
       if (tx_header->tp_status == TP_STATUS_SEND_REQUEST) tosend = true;
@@ -134,6 +141,7 @@ void main(int argc, char **argv) {
       total_bytes += ec_send;
       printf("send %d packets (+%d bytes)\n", total_pkts, total_bytes ); fflush(stdout);
     }
+*/
   }
 
   struct tpacket_stats_v3 stats;
