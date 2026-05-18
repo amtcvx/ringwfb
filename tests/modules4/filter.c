91be8146d13a229d96f4e4c0cae52941c788a8bf
pprz <Vous@exemple.com>
2026-05-18 19:43:42 +0200

    up

--- tests/modules4/filter.c
+++ tests/modules4/filter.c
@@ -8,47 +8,33 @@
 #include <linux/string.h>
 #include <linux/byteorder/generic.h>
 
-static unsigned char *ip_address = "\xC0\xA8\x00\x01"; 
-static char *interface = "lo";                          
-unsigned char *port = "\x00\x17";                       
+uint32_t localhost_IntIP = 16777343; // "127.0.0.1"
+uint16_t destport = 5600;
 
 struct sk_buff *sock_buff;                              
 static struct nf_hook_ops *nf_filter_ops = NULL;
 static struct nf_hook_ops *nf_filter_out_ops = NULL;
 
+/******************************************************************************/
 static unsigned int nf_filter_handler(void *priv, struct sk_buff *skb, const struct nf_hook_state *state) {
-//  if(strcmp(in->name,interface) == 0){ return NF_DROP; }     
-        
+
   if(skb==NULL) return NF_ACCEPT;
   struct iphdr * iph;
   iph = ip_hdr(skb);
-
   if(iph && iph->protocol == IPPROTO_UDP) {
-    pr_info("source : %p | dest : %p\n", &(iph->saddr),&(iph->saddr));
+    //pr_info("source : %pI4 | dest : %pI4 | %d  ", &(iph->saddr),&(iph->daddr),iph->saddr);
+    struct udphdr *udph = udp_hdr(skb); 
+    //pr_info("source port : %hu | dest port : %hu\n", ntohs(udph->source),ntohs(udph->dest));
+    if ((localhost_IntIP == iph->saddr) && (localhost_IntIP == iph->daddr) &&  (ntohs(udph->dest)== destport)) {
+      pr_info("len : %hu\n", ntohs(udph->len));
+    }
   }
   return NF_ACCEPT;
 }
-//  sock_buff = *skb;
-//  if(!sock_buff){ return NF_ACCEPT; }                   
-//  if(!(sock_buff->nh.iph)){ return NF_ACCEPT; }              
-//  if(sock_buff->nh.iph->saddr == *(unsigned int*)ip_address){ return NF_DROP; }
-//
-//if(sock_buff->nh.iph->protocol != 17){ return NF_ACCEPT; }                 
-//udp_header = (struct udphdr *)(sock_buff->data + (sock_buff->nh.iph->ihl *4)); 
-//if((udp_header->dest) == *(unsigned short*)port){ return NF_DROP; }
-//return NF_ACCEPT;
-/*
-  if(skb==NULL) return NF_ACCEPT;
-  struct iphdr * iph;
-  iph = ip_hdr(skb);
-  if(iph && iph->protocol == IPPROTO_TCP) {
-    struct tcphdr *tcph = tcp_hdr(skb);
-    pr_info("source : %pI4:%hu | dest : %pI4:%hu | seq : %u | ack_seq : %u | window : %hu | csum : 0x%hx | urg_ptr %hu\n", &(iph->saddr),ntohs(tcph->source),&(iph->saddr),ntohs(tcph->dest), ntohl(tcph->seq), ntohl(tcph->ack_seq), ntohs(tcph->window), ntohs(tcph->check), ntohs(tcph->urg_ptr));
-  }
-  return NF_ACCEPT;
-*/
 
+/******************************************************************************/
 static int __init nf_filter_init(void) {
+
   nf_filter_ops = (struct nf_hook_ops*)kcalloc(1,  sizeof(struct nf_hook_ops), GFP_KERNEL);
   if(nf_filter_ops!=NULL) {
     nf_filter_ops->hook = (nf_hookfn*)nf_filter_handler;
@@ -68,7 +54,9 @@ static int __init nf_filter_init(void) {
  return 0;
 }
 
+/******************************************************************************/
 static void __exit nf_filter_exit(void) {
+
   if(nf_filter_ops != NULL) {
     nf_unregister_net_hook(&init_net, nf_filter_ops);
     kfree(nf_filter_ops);
@@ -80,8 +68,7 @@ static void __exit nf_filter_exit(void) {
   }
 }
 
+/******************************************************************************/
 module_init(nf_filter_init);
 module_exit(nf_filter_exit);
-
 MODULE_LICENSE("GPL");
-
