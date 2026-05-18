f97458d917946d4d8510e54388fdb2d8dba0b14a
pprz <Vous@exemple.com>
2026-05-18 19:55:44 +0200

    up

--- tests/modules4/filter.c
+++ tests/modules4/filter.c
@@ -13,7 +13,6 @@ uint16_t destport = 5600;
 
 struct sk_buff *sock_buff;                              
 static struct nf_hook_ops *nf_filter_ops = NULL;
-static struct nf_hook_ops *nf_filter_out_ops = NULL;
 
 /******************************************************************************/
 static unsigned int nf_filter_handler(void *priv, struct sk_buff *skb, const struct nf_hook_state *state) {
@@ -43,14 +42,6 @@ static int __init nf_filter_init(void) {
     nf_filter_ops->priority = NF_IP_PRI_FIRST;
     nf_register_net_hook(&init_net, nf_filter_ops);
   }
-  nf_filter_out_ops = (struct nf_hook_ops*)kcalloc(1, sizeof(struct nf_hook_ops), GFP_KERNEL);
-  if(nf_filter_out_ops!=NULL) {
-    nf_filter_out_ops->hook = (nf_hookfn*)nf_filter_handler;
-    nf_filter_out_ops->hooknum = NF_INET_LOCAL_OUT;
-    nf_filter_out_ops->pf = PF_INET; // NFPROTO_IPV4;
-    nf_filter_out_ops->priority = NF_IP_PRI_FIRST;
-    nf_register_net_hook(&init_net, nf_filter_out_ops);
- }
  return 0;
 }
 
@@ -60,11 +51,6 @@ static void __exit nf_filter_exit(void) {
   if(nf_filter_ops != NULL) {
     nf_unregister_net_hook(&init_net, nf_filter_ops);
     kfree(nf_filter_ops);
-  }
-
-  if(nf_filter_out_ops != NULL) {
-    nf_unregister_net_hook(&init_net, nf_filter_out_ops);
-    kfree(nf_filter_out_ops);
   }
 }
 
