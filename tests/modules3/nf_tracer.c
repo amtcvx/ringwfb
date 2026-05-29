1de69d12a5119a31254c3a76290d030ba1fc3f51
pprz <Vous@exemple.com>
2026-05-29 16:11:25 +0200

    up

--- tests/modules3/nf_tracer.c
+++ tests/modules3/nf_tracer.c
@@ -3,12 +3,15 @@
 #include <linux/udp.h>
 #include <linux/inet.h>
 
+uint8_t *wifiname = "enp5s0";//"wlx3c7c3fa9bdca";
+
 static struct nf_hook_ops *nf_tracer_ops = NULL;
 static struct nf_hook_ops *nf_tracer_out_ops = NULL;
 
 typedef struct {
-  uint32_t ip;
+  struct in_addr ipint;
 } priv_t;
+
 static priv_t mypriv;
 
 static unsigned int nf_tracer_handler(void *priv, struct sk_buff *skb, const struct nf_hook_state *state) {
@@ -23,7 +26,11 @@ static unsigned int nf_tracer_handler(void *priv, struct sk_buff *skb, const str
 
 //   if (iph->daddr == ((priv_t *)priv)->ip) {
 
-     pr_info("%pI4 In len(%d)(%d)  %pI4 |  %pI4 | %hu | %hu \n", mypriv.ip,
+
+//     priv = (void *)&mypriv;
+
+//     pr_info("%pI4 In len(%d)(%d)  %pI4 |  %pI4 | %hu | %hu \n",(priv_t *)priv->ipint,
+     pr_info("In len(%d)(%d)  %pI4 |  %pI4 | %hu | %hu \n",
 		   skb->len, ntohs(udph->len),
 		   &(iph->saddr),&(iph->daddr),
 		   ntohs(udph->source),ntohs(udph->dest));
@@ -36,11 +43,15 @@ static unsigned int nf_tracer_handler(void *priv, struct sk_buff *skb, const str
 
 static int __init nf_tracer_init(void) {
 
-  in4_pton("192.168.3.200", 13, (u8 *)&(mypriv.ip), '\n', NULL);
+  struct net_device *wifidev = dev_get_by_name(&init_net,wifiname);
+
+  in4_pton("192.168.3.200", 13, (u8 *)&(mypriv.ipint), '\n', NULL);
 
   nf_tracer_ops = (struct nf_hook_ops*)kcalloc(1,  sizeof(struct nf_hook_ops), GFP_KERNEL);
   if(nf_tracer_ops!=NULL) {
     nf_tracer_ops->hook = (nf_hookfn*)nf_tracer_handler;
+//    nf_tracer_ops->priv = (void *)&mypriv;
+//    nf_tracer_ops->dev = wifidev;
     nf_tracer_ops->hooknum = NF_INET_PRE_ROUTING;
     nf_tracer_ops->pf = NFPROTO_IPV4;
     nf_tracer_ops->priority = NF_IP_PRI_FIRST;
@@ -50,6 +61,8 @@ static int __init nf_tracer_init(void) {
   nf_tracer_out_ops = (struct nf_hook_ops*)kcalloc(1, sizeof(struct nf_hook_ops), GFP_KERNEL);
   if(nf_tracer_out_ops!=NULL) {
     nf_tracer_out_ops->hook = (nf_hookfn*)nf_tracer_handler;
+//    nf_tracer_ops->priv = (void *)&mypriv;
+//    nf_tracer_ops->dev = wifidev;
     nf_tracer_out_ops->hooknum = NF_INET_LOCAL_OUT;
     nf_tracer_out_ops->pf = NFPROTO_IPV4;
     nf_tracer_out_ops->priority = NF_IP_PRI_FIRST;
