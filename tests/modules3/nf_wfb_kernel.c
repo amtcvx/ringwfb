623ac4cd4a94e96ec9cac97b1300179b4b3e5a2f
pprz <Vous@exemple.com>
2026-06-01 17:44:37 +0200

    up

--- tests/modules3/nf_wfb_kernel.c
+++ tests/modules3/nf_wfb_kernel.c
@@ -8,17 +8,20 @@
 uint8_t *wifiname = "enp5s0";//"wlx3c7c3fa9bdca";
 uint16_t destport = 5600;
 
+static uint32_t ip1;
 
 /******************************************************************************/
 static struct nf_hook_ops *nf_wfb_hook_pre_routing = NULL;
-static struct nf_hook_ops *nf_wfb_hook_local_in = NULL;
 /*
+static struct nf_hook_ops *nf_wfb_hook_local_in = NULL;
 static struct nf_hook_ops *nf_wfb_hook_local_out = NULL;
+*/
 static struct nf_hook_ops *nf_wfb_hook_post_routing = NULL;
-i*/
+
 typedef struct {
   uint32_t localipint;
   struct net_device *localdev;
+  struct net_device *wifidev;
 } priv_t;
 
 static priv_t mypriv;
@@ -31,7 +34,7 @@ static unsigned int nf_wfb_handler_pre_routing(void *priv, struct sk_buff *skb, 
     iph = ip_hdr(skb);
     if(iph && iph->protocol == IPPROTO_UDP) {
       struct udphdr *udph = udp_hdr(skb);
-      if ((mypriv.localipint == iph->daddr) &&  (ntohs(udph->dest)== destport)) {
+      if ((mypriv.localipint != iph->saddr) && (mypriv.localipint == iph->daddr) &&  (ntohs(udph->dest)== destport)) {
 
         pr_info("PRE len(%d)(%d)  %pI4 |  %pI4 | %hu | %hu \n",
 		   skb->len, ntohs(udph->len),
@@ -49,6 +52,7 @@ static unsigned int nf_wfb_handler_pre_routing(void *priv, struct sk_buff *skb, 
 }
 
 /******************************************************************************/
+/*
 static unsigned int nf_wfb_handler_local_in(void *priv, struct sk_buff *skb, const struct nf_hook_state *state) {
 
   if(skb != NULL) {
@@ -64,7 +68,7 @@ static unsigned int nf_wfb_handler_local_in(void *priv, struct sk_buff *skb, con
   }
   return NF_ACCEPT;
 }
-/*
+
 static unsigned int nf_wfb_handler_local_out(void *priv, struct sk_buff *skb, const struct nf_hook_state *state) {
 
   if(skb != NULL) {
@@ -80,7 +84,7 @@ static unsigned int nf_wfb_handler_local_out(void *priv, struct sk_buff *skb, co
   }
   return NF_ACCEPT;
 }
-
+*/
 static unsigned int nf_wfb_handler_post_routing(void *priv, struct sk_buff *skb, const struct nf_hook_state *state) {
 
   if(skb != NULL) {
@@ -88,21 +92,55 @@ static unsigned int nf_wfb_handler_post_routing(void *priv, struct sk_buff *skb,
     iph = ip_hdr(skb);
     if(iph && iph->protocol == IPPROTO_UDP) {
       struct udphdr *udph = udp_hdr(skb);
-      pr_info("POST len(%d)(%d)  %pI4 |  %pI4 | %hu | %hu \n",
+      if ((mypriv.localipint == iph->saddr) && (mypriv.localipint == iph->daddr) &&  (ntohs(udph->dest)== destport)) {
+        struct sk_buff * nskb = skb_clone(skb, GFP_KERNEL);
+        pskb_expand_head(nskb, sizeof(struct ethhdr), 0, GFP_KERNEL);
+
+        skb_pull_data(nskb,28);
+
+        struct udphdr* nuh = (struct udphdr*)skb_push(nskb, sizeof(struct udphdr));
+        nuh->source = htons(59976);
+        nuh->dest = htons(5600);
+
+        struct iphdr* niph = (struct iphdr*)skb_push(nskb, sizeof(struct iphdr));
+        long unsigned int dum = sizeof(struct iphdr) / 4;
+        niph->ihl = dum;
+        niph->version = 4;
+        niph->protocol = IPPROTO_UDP;
+        niph->saddr = ip1;
+        niph->daddr = mypriv.localipint;
+
+        nskb->dev = mypriv.wifidev;
+        nskb->pkt_type = PACKET_OUTGOING;
+
+        struct ethhdr* neth = (struct ethhdr*)skb_push(nskb, sizeof (struct ethhdr));//add data to the start of a buffer
+        nskb->protocol = neth->h_proto = htons(ETH_P_IP);
+
+        char destaddr[ETH_ALEN] = {0x90,0x1b,0xe,0x61,0x39,0x4f};
+        memcpy(neth->h_dest, destaddr, ETH_ALEN);
+        memcpy(neth->h_source, nskb->dev->dev_addr, ETH_ALEN);
+
+        int ret = dev_queue_xmit(nskb);
+        pr_info("ret(%d)\n",ret);
+
+        pr_info("POST len(%d)(%d)  %pI4 |  %pI4 | %hu | %hu \n",
 		   skb->len, ntohs(udph->len),
 		   &(iph->saddr),&(iph->daddr),
 		   ntohs(udph->source),ntohs(udph->dest));
+      }
     }
   }
   return NF_ACCEPT;
 }
-*/
+
 /******************************************************************************/
 static int __init nf_wfb_kernel_init(void) {
 
-  struct net_device *wifidev = dev_get_by_name(&init_net,wifiname);
   mypriv.localdev = dev_get_by_name(&init_net, "lo");
+  mypriv.wifidev = dev_get_by_name(&init_net, wifiname);
   in4_pton("127.0.0.1", 9, (u8 *)&(mypriv.localipint), '\n', NULL);
+
+  in4_pton("192.168.3.100", 13, (u8 *)&ip1, '\n', NULL);
 
   nf_wfb_hook_pre_routing = (struct nf_hook_ops*)kcalloc(1,  sizeof(struct nf_hook_ops), GFP_KERNEL);
   if(nf_wfb_hook_pre_routing != NULL) {
@@ -115,7 +153,7 @@ static int __init nf_wfb_kernel_init(void) {
 
     nf_register_net_hook(&init_net, nf_wfb_hook_pre_routing);
   }
-
+/*
   nf_wfb_hook_local_in = (struct nf_hook_ops*)kcalloc(1, sizeof(struct nf_hook_ops), GFP_KERNEL);
   if(nf_wfb_hook_local_in != NULL) {
     nf_wfb_hook_local_in->hook = (nf_hookfn*)nf_wfb_handler_local_in;
@@ -127,7 +165,7 @@ static int __init nf_wfb_kernel_init(void) {
 
     nf_register_net_hook(&init_net, nf_wfb_hook_local_in);
   }
-/*
+
   nf_wfb_hook_local_out = (struct nf_hook_ops*)kcalloc(1, sizeof(struct nf_hook_ops), GFP_KERNEL);
   if(nf_wfb_hook_local_out != NULL) {
     nf_wfb_hook_local_out->hook = (nf_hookfn*)nf_wfb_handler_local_out;
@@ -139,7 +177,7 @@ static int __init nf_wfb_kernel_init(void) {
 
     nf_register_net_hook(&init_net, nf_wfb_hook_local_out);
   }
-
+*/
   nf_wfb_hook_post_routing = (struct nf_hook_ops*)kcalloc(1,  sizeof(struct nf_hook_ops), GFP_KERNEL);
   if(nf_wfb_hook_post_routing != NULL) {
     nf_wfb_hook_post_routing->hook = (nf_hookfn*)nf_wfb_handler_post_routing;
@@ -151,7 +189,7 @@ static int __init nf_wfb_kernel_init(void) {
 
     nf_register_net_hook(&init_net, nf_wfb_hook_post_routing);
   }
-*/
+
   return 0;
 }
 
@@ -162,12 +200,12 @@ static void __exit nf_wfb_kernel_exit(void) {
     nf_unregister_net_hook(&init_net, nf_wfb_hook_pre_routing);
     kfree(nf_wfb_hook_pre_routing);
   }
-
+/*
   if(nf_wfb_hook_local_in != NULL) {
     nf_unregister_net_hook(&init_net, nf_wfb_hook_local_in);
     kfree(nf_wfb_hook_local_in);
   }
-/*
+
   if(nf_wfb_hook_local_out != NULL) {
     nf_unregister_net_hook(&init_net, nf_wfb_hook_local_out);
     kfree(nf_wfb_hook_local_out);
