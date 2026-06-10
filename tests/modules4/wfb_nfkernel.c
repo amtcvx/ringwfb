c2a1f1af4cf04c769821257643142a7d2bc2349d
pprz <you@example.com>
2026-06-10 11:48:20 +0200

    up

--- tests/modules4/wfb_nfkernel.c
+++ tests/modules4/wfb_nfkernel.c
@@ -14,7 +14,7 @@ gst-launch-1.0 udpsrc port=5600 ! application/x-rtp, encoding-name=H265, payload
 
 /******************************************************************************/
 uint8_t *localname = "lo";
-uint8_t *wifiname = "enp5s0";//"wlx3c7c3fa9bdca";
+uint8_t *wifiname = "enx00e04c360616";//"wlx3c7c3fa9bdca";
 uint16_t outdestport = 5600, indestport = 5700;
 
 static uint32_t ip1,ip2;
@@ -66,44 +66,43 @@ static unsigned int wfb_nfkernel_handler_post(void *priv, struct sk_buff *skb, c
 static rx_handler_result_t handle_frame(struct sk_buff **pskb) {
 
   struct sk_buff *skb = *pskb;
+  skb = skb_share_check(skb, GFP_ATOMIC);
 
-  if(skb != NULL) {
+  if (!skb) return RX_HANDLER_CONSUMED;
 
-    if (skb->len > 0) {
-      struct iphdr * iph;
-      iph = ip_hdr(skb);
+  *pskb = skb;
 
-      if ((iph->version != 4) || (iph->protocol != IPPROTO_UDP)) return RX_HANDLER_CONSUMED;
+  struct iphdr * iph;
+  iph = ip_hdr(skb);
 
-      uint8_t *ptr = skb_network_header(skb);
-      struct udphdr *udph = (struct udphdr *)(ptr + sizeof(struct iphdr));// udp_hdr(skb);
+  if ((iph->version != 4) || (iph->protocol != IPPROTO_UDP)) return RX_HANDLER_PASS;
 
-      if ((ntohs(udph->dest) != outdestport)) return RX_HANDLER_CONSUMED;
+  uint8_t *ptr = skb_network_header(skb);
+  struct udphdr *udph = (struct udphdr *)(ptr + sizeof(struct iphdr));// udp_hdr(skb);
 
-      pr_info("POST out skb->len(%d) ips(%pI4) ipd(%pI4) ulen(%hu) ups(%hu) upd(%hu) \n",
-          skb->len,
-          &(iph->saddr), &(iph->daddr),
-          ntohs(udph->len),
-          ntohs(udph->source), ntohs(udph->dest));
+  if ((ntohs(udph->dest) != outdestport)) return RX_HANDLER_PASS;
 
-      uint8_t ch, *p;
-      p = skb->data;
-      for (uint16_t i = 0; i < skb->len; i++) {
-        if (i == sizeof(struct iphdr) + sizeof(struct udphdr)) printk(KERN_CONT "In pay\n");
-        ch = p[i];
-        printk(KERN_CONT "%02x ", (uint32_t) ch);
-      }
-      printk(KERN_CONT "\n");
-
-
-      *pskb = skb;
-      skb->dev = mypriv.localdev;
-      udph->dest = htons(indestport);
-
-      return RX_HANDLER_ANOTHER;
-    }
+  uint8_t ch, *p;
+  p = skb->data;
+  for (uint16_t i = 0; i < skb->len; i++) {
+    if (i == sizeof(struct iphdr) + sizeof(struct udphdr)) printk(KERN_CONT "In pay\n");
+    ch = p[i];
+    printk(KERN_CONT "%02x ", (uint32_t) ch);
   }
-  return RX_HANDLER_CONSUMED;
+  printk(KERN_CONT "\n");
+
+  udph->dest = htons(indestport);
+
+  skb->dev = mypriv.localdev;
+  skb->pkt_type = PACKET_HOST;
+
+  pr_info("POST out skb->len(%d) ips(%pI4) ipd(%pI4) ulen(%hu) ups(%hu) upd(%hu) \n",
+    skb->len,
+    &(iph->saddr), &(iph->daddr),
+    ntohs(udph->len),
+    ntohs(udph->source), ntohs(udph->dest));
+
+  return RX_HANDLER_ANOTHER;
 }
 
 /******************************************************************************/
@@ -114,8 +113,8 @@ static int __init wfb_nfkernel_init(void) {
 
   in4_pton("127.0.0.1", 9, (u8 *)&(mypriv.localipint), '\n', NULL);
 
-  in4_pton("192.168.3.100", 13, (u8 *)&ip1, '\n', NULL);
-  in4_pton("192.168.3.200", 13, (u8 *)&ip2, '\n', NULL);
+  in4_pton("192.168.3.200", 13, (u8 *)&ip1, '\n', NULL);
+  in4_pton("192.168.3.100", 13, (u8 *)&ip2, '\n', NULL);
 
   dev_set_promiscuity(mypriv.wifidev,1);
   netdev_rx_handler_register(mypriv.wifidev, handle_frame, NULL);
