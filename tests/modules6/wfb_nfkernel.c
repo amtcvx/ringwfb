2fe9cb4db1a289727972cf64cf732444210cfad6
pprz <Vous@exemple.com>
2026-06-29 14:12:02 +0200

    up

--- tests/modules6/wfb_nfkernel.c
+++ tests/modules6/wfb_nfkernel.c
@@ -14,7 +14,7 @@ gst-launch-1.0 udpsrc port=5700 ! application/x-rtp, encoding-name=H265, payload
 
 /******************************************************************************/
 uint8_t *localname = "lo";
-uint8_t *wifiname = "eno1";
+uint8_t *wifiname = "enp5s0";
 uint16_t outdestport = 5600, indestport = 5700;
 
 typedef struct {
@@ -43,8 +43,7 @@ static unsigned int wfb_nfkernel_handler_post(void *priv, struct sk_buff *skb, c
     if(iph && iph->protocol == IPPROTO_UDP) {
       struct udphdr *udph = udp_hdr(skb);
       if ((mypriv.localipint == iph->saddr) && (mypriv.localipint == iph->daddr) &&  (ntohs(udph->dest)== outdestport)) {
-
-
+/*
         uint8_t ch, *p;
         pr_info("POST in len(%d)\n",skb->len);
         p = skb->data;
@@ -53,28 +52,16 @@ static unsigned int wfb_nfkernel_handler_post(void *priv, struct sk_buff *skb, c
           printk(KERN_CONT "%02x ", (uint32_t) ch);
         }
         printk(KERN_CONT "\n");
-
+*/
 
         struct sk_buff * nskb = skb_clone(skb, GFP_KERNEL);
-
-//        pskb_expand_head(nskb, sizeof(struct ethhdr), 0, GFP_KERNEL);
-
-        pskb_expand_head(nskb, sizeof(struct ethhdr) + sizeof(payhd_t), 0, GFP_KERNEL);
-
-        skb_pull_data(nskb,sizeof(struct iphdr)+sizeof(struct udphdr)); 
-
-        payhd_t* pah = (payhd_t*)skb_push(nskb, sizeof(payhd_t));
-        pah->droneid = 1;
-        pah->seq =2;
-        pah->msglen = 3;
-        pah->backfreq = 4;
+        pskb_expand_head(nskb, sizeof(struct ethhdr), 0, GFP_KERNEL);
 
         struct iphdr* niph = ip_hdr(nskb);
 	memset(&niph->saddr, 0, sizeof(niph->saddr));
 	memset(&niph->daddr, 0, sizeof(niph->saddr));
 
-        struct iphdr* iph = ip_hdr(skb);
-        niph->tot_len = iph->tot_len + ntohs(sizeof(payhd_t));
+        niph->tot_len += ntohs(sizeof(payhd_t));
 
         nskb->dev = mypriv.wifidev;
         nskb->pkt_type = PACKET_OUTGOING;
@@ -88,16 +75,21 @@ static unsigned int wfb_nfkernel_handler_post(void *priv, struct sk_buff *skb, c
         uint8_t *nptr = skb_network_header(nskb);
         struct udphdr *nudph = (struct udphdr *)(nptr + sizeof(struct iphdr));
 
-	struct uphdr* uh = ip_hdr(skb);
-	nuh->len = uh->len + ntohs(sizeof(payhd_t));
+        nudph->len += ntohs(sizeof(payhd_t));
 
-/*
-        pr_info("POST out skb->len(%d) ips(%pI4) ipd(%pI4) ulen(%hu) ups(%hu) upd(%hu) \n",
-          nskb->len,
+        payhd_t* pah = (payhd_t*)skb_put(nskb, sizeof(payhd_t));
+        pah->droneid = 1;
+        pah->seq =2;
+        pah->msglen = 3;
+        pah->backfreq = 4;
+
+        pr_info("POST out tot_len(%hu) ips(%pI4) ipd(%pI4) ulen(%hu) ups(%hu) upd(%hu) \n",
+          ntohs(niph->tot_len),
           &(niph->saddr), &(niph->daddr),
           ntohs(nudph->len),
           ntohs(nudph->source), ntohs(nudph->dest));
-*/
+
+/*
         pr_info("POST out len(%d)\n",nskb->len);
         p = nskb->data;
         for (uint16_t i = 0; i < nskb->len; i++) {
@@ -105,8 +97,7 @@ static unsigned int wfb_nfkernel_handler_post(void *priv, struct sk_buff *skb, c
           printk(KERN_CONT "%02x ", (uint32_t) ch);
         }
         printk(KERN_CONT "\n");
-
-
+*/
         dev_queue_xmit(nskb);
       }
     }
